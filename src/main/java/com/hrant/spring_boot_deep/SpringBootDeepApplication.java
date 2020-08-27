b7cc26f1bdf9154d6806dabdb22ff18f9d2c4eae
hrant2001 <30405316+hrant2001@users.noreply.github.com>
2020-08-27 15:42:46 +0400

    Spring boot and PostgreSQL

---
+++ src/main/java/com/hrant/spring_boot_deep/SpringBootDeepApplication.java
@@ -0,0 +1,13 @@
+package com.hrant.spring_boot_deep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDeepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hrant/spring_boot_deep/SpringBootDeepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrant.spring_boot_deep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDeepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
